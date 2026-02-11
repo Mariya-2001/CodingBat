f79272711772ebab17f821a8e0b5eb915b28457d
Mariya <lamkia2001@gmail.com>
2026-02-11 11:33:16 +0400

    Sleep-in in Warmup1 folder solved

---
+++ sleepin.java
@@ -0,0 +1,10 @@
+public class sleepin {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}

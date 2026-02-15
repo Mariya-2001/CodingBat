5c1c4f6f3ffc4ffeed274f9089918737239cb091
Mariya <lamkia2001@gmail.com>
2026-02-15 08:57:45 +0400

    diff21 in Warmup1 folder solved

--- sleepin.java
+++ sleepin.java
@@ -1,10 +1,19 @@
 public class sleepin {
-    public boolean sleepIn(boolean weekday, boolean vacation) {
+    public boolean sleepin(boolean weekday, boolean vacation) {
         if (!weekday || vacation){
             return true;
         }else{
             return false;
         }
     }
+
+    public static void main(String[] args){
+        sleepin s = new sleepin();
+
+        System.out.println(s.sleepin(false,false));
+        System.out.println(s.sleepin(true,false));
+        System.out.println(s.sleepin(false,true));
+    }
+
 
 }

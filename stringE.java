d3e910ef6f4915431b468c77dbbf64dd63edd8ea
Mariya <lamkia2001@gmail.com>
2026-02-15 16:24:35 +0400

    stringE in Warmup1 folder solved

---
+++ stringE.java
@@ -0,0 +1,18 @@
+public class stringE {
+    public static void main(String[] args){
+        System.out.println(stringE("Hee")); //true
+        System.out.println(stringE("He")); //true
+        System.out.println(stringE("eeee")); //false
+    }
+    public static boolean stringE(String str){
+        int count = 0; // initiate the number of e to 0
+
+        for (char c : str.toCharArray()) {
+            if (c == 'e') {
+                count++;
+            }
+        }
+        return (count >= 1 && count <= 3);
+    }
+
+}

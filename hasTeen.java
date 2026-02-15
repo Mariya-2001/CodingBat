21b985b5b54b9a21d05be10a552da86dff134eff
Mariya <lamkia2001@gmail.com>
2026-02-15 15:05:20 +0400

    hasTeen in Warmup1 folder solved

---
+++ hasTeen.java
@@ -0,0 +1,11 @@
+public class hasTeen {
+    public static void main(String[] args){
+        System.out.println(hasTeen(13,15,17));
+        System.out.println(hasTeen(10,12,13));
+    }
+    public static boolean hasTeen(int x , int y , int z){
+        return (x >= 13 && x <=19) ||
+                (y >= 13 && y <=19) ||
+                (z >= 13 && z <=19);
+    }
+}

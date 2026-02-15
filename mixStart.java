c40d64ba66068db1d9f3fc61cb1fb163f621f76f
Mariya <lamkia2001@gmail.com>
2026-02-15 15:39:54 +0400

    mixStart in Warmup1 folder solved

---
+++ mixStart.java
@@ -0,0 +1,12 @@
+public class mixStart {
+    public static void main(String[] args){
+        System.out.println(mixStart("mix mm"));
+        System.out.println(mixStart("pix nn"));
+        System.out.println(mixStart("ix"));
+    }
+    public static boolean mixStart(String str){
+        return str.length() >= 3 && str.substring(1,3).equals("ix");
+        // length should be equal or more than 3 +
+        // the second and third letter should contain ix (starts in index 1 and stops in index 3)
+    }
+}

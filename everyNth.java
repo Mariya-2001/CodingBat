0b701ef35438c30595a79e2e34ff35c8a854d362
Mariya <lamkia2001@gmail.com>
2026-02-15 16:34:35 +0400

    everyNth in Warmup1 folder solved

---
+++ everyNth.java
@@ -0,0 +1,15 @@
+public class everyNth {
+    public static void main(String[] args){
+        System.out.println(everyNth("abcdesf",2)); //acef
+        System.out.println(everyNth("nbfdsa",2)); //nfs
+        System.out.println(everyNth("poiuyt",3)); //pu
+    }
+    public static String everyNth(String str, int n){
+        StringBuilder res = new StringBuilder();
+
+        for (int i = 0; i< str.length();i+=n){
+            res.append(str.charAt(i));
+        }
+        return res.toString();
+    }
+}

b7024d32d230b62f421b0850ff537aca65277ff1
Mariya <lamkia2001@gmail.com>
2026-02-15 10:47:51 +0400

    backAround in Warmup1 folder solved

---
+++ backAround.java
@@ -0,0 +1,12 @@
+public class backAround {
+    public static void main(String[] args){
+        System.out.println(backAround("Cat"));
+        System.out.println(backAround("hello"));
+        System.out.println(backAround("a"));
+    }
+    public static String backAround(String str){
+        char lastCharacter = str.charAt(str.length() - 1);
+
+        return lastCharacter + str + lastCharacter;
+    }
+}

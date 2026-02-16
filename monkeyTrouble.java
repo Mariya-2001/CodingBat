75c83cc2510f030b4882466e9ac881b8c1cc21a1
Mariya <lamkia2001@gmail.com>
2026-02-16 12:04:11 +0400

    monkeyTrouble in Warmup1 folder solved

---
+++ monkeyTrouble.java
@@ -0,0 +1,11 @@
+public class monkeyTrouble {
+    public static void main(String[] args){
+        System.out.println(monkeyTrouble(true,true));
+        System.out.println(monkeyTrouble(false,false));
+        System.out.println(monkeyTrouble(true,false));
+        System.out.println(monkeyTrouble(false,true));
+    }
+    public static boolean monkeyTrouble(boolean asmile,boolean bsmile){
+        return asmile == bsmile; // true ,true ,false ,false
+    }
+}

463f562ed1cc250166178446bb5c4ed2db1c9f8a
Mariya <lamkia2001@gmail.com>
2026-02-16 12:30:37 +0400

    posNeg in Warmup1 folder solved

---
+++ posNeg.java
@@ -0,0 +1,14 @@
+public class posNeg {
+    public static void main(String[] args){
+        System.out.println(posNeg(2,-2,true));
+        System.out.println(posNeg(-2,2,true));
+        System.out.println(posNeg(-5,-2,false));
+    }
+    public static boolean posNeg(int x, int y , boolean neg){
+        if(neg) {
+            return (x < 0 && y < 0);
+        } else{
+            return ((x < 0 && y <0) || x > 0 && y < 0);
+        }
+    }
+}

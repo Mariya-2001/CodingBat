a00b50ce09a810a0813069f482c25bd4fe84e155
Mariya <lamkia2001@gmail.com>
2026-03-10 10:12:20 +0400

    Solved Exercise icyHot in Warmup1

--- Warmup1/icyHot.java
+++ Warmup1/icyHot.java
@@ -3,6 +3,12 @@ package Warmup1;
 public class icyHot {
     public static void main(String[] args){
 
-        System.out.println();
+        System.out.println(icyHot(120,-1)); // return false
+        System.out.println(icyHot(-2,110)); // return true
+        System.out.println(icyHot(3,129)); // return false
+        }
+
+        public static boolean icyHot(int temprature1, int temprature2){
+        return ((temprature1 < 0 && temprature2 > 100 )|| (temprature1 < 100 && temprature2 < 0));
     }
 }
